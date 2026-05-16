fb05d53f88340eba7ee4e33d4f7b0d842718ac9f
Filip Rozanowski <filip.rozanowski@gmail.com>
2026-05-17 00:10:28 +0200

    continue

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ langBtns.forEach(btn => {
     btn.classList.add('active');
   });
 });
+
+const tabBtns = document.querySelectorAll('.tab_btn');
+const panels = document.querySelectorAll('.project_panel');
+
+tabBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const target = btn.dataset.project;
+
+        tabBtns.forEach(b => b.classList.remove('active'));
+        panels.forEach(p => p.classList.remove('active'));
+
+        btn.classList.add('active');
+        document.querySelector(`.project_panel[data-project="${target}"]`).classList.add('active');
+    });
+});
